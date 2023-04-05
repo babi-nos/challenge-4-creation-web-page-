935ed3a3b596217217ad9943c58e578997ed039b
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-04-05 12:55:17 +0200

    Challenge 4 web page site vr  not responsive

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// const cards = document.querySelectorAll(".cards");
+// const image = document.querySelectorAll(".img-cards");
+// const titleCards = document.querySelector(".title-cards");
+
+// // cards.addEventListener('mouseover', () => {
+// //     console.log("yesssssssssssssssssssssssssssss");
+// // })
+
+
+// cards.forEach( (cards) => {
+//     cards.addEventListener('mouseover', () => {
+//         cards.style.cursor = "pointer";
+//         image.forEach((image) => {
+//             image.style.opacity = "0.55";
+//         })  
+//     });
+// })
+
+
+
+// si mon cads est hover alors img sera opacity 0.55 et mon h1 sera black opacity 1
